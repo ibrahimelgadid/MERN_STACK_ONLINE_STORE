000f68ef250020b700bb219aabc00fc64d807f1f
ibrahimelgadid <ibrahimelgadid30@gmail.com>
2022-05-05 21:44:57 +0200

    dhcgss

--- server.js
+++ server.js
@@ -96,3 +96,8 @@ io.on("connection", (socket) => {
   });
 });
 /////////////////////////////////////////
+
+function strCount(str, letter) {
+  return str.split(letter).length - 1;
+}
+console.log(strCount("Hello", "l"));
